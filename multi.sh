3c3bf6869912062c306c4469e29b7d82de1b4792
jtfield <jtoscanifield@ucmerced.edu>
2018-07-03 16:38:23 -0700

    added script practicing allowing user input for defining how many programs to run

---
+++ multi.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+while getopts ":e:h" opt; do
+	case $opt in
+		e) END="$OPTARG" ;;
+		h) echo "-e is the ending number of the sequence" ;;
+		\?) echo "invalid option -$OPTARG" >&2 ;;
+	esac
+done
+
+#for i in $(seq 1 $END); do echo $i; done
+
+for i in $(seq 1 $END);
+do
+	dd if=/dev/zero of=/dev/null &
+	#stress --cpu 1 --timeout 60 &
+
+done
